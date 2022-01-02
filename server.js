7d415d838cc0742098d6ca19863b897f077d1956
tiana <tianasjohnson98@gmail.com>
2022-01-02 17:54:11 -0500

    post notes

--- server.js
+++ server.js
@@ -16,3 +16,46 @@ app.use(express.json());
 
 let uniqueIDs = [];
 let localNotes = [];
+
+//routes for HtML
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+  data = req.body;
+  let id = uniqueID();
+
+  let note = new Note(data.title, data.text, id);
+  localNotes = [];
+  localNotes.push(note);
+  fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let database = JSON.parse(data);
+      localNotes = localNotes.concat(database);
+    }
+
+    fs.writeFile(
+      path.join(__dirname, "/db/db.json"),
+      JSON.stringify(localNotes),
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  });
+
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
